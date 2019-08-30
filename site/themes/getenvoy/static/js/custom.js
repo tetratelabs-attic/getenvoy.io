1d43adc219c7c55226190057168befe0ac9e634b
Adityo Pratomo <pratomo.adityo@gmail.com>
2019-08-30 17:20:27 +0700

    Fix auto copy paste on code box (#105)
    * fix auto copy paste on code box
    
    * Signed-off-by Adityo Pratomo adityo@tetrate.io
    
    Signed-off-by: Adityo Pratomo <pratomo.adityo@gmail.com>

--- site/themes/getenvoy/static/js/custom.js
+++ site/themes/getenvoy/static/js/custom.js
@@ -1,58 +1,65 @@
 $(function() {
-    function clipboardMagic(){
+  function clipboardMagic() {
+    $('pre code.language-sh').append(
+      '<button><img src="/images/copy.png" /></button>'
+    );
 
-        // find all the "code" blocks and bind click event...
-        $("pre code").on('click',function(){
-            var $this = $(this);
+    // find all the "code" blocks and bind click event...
+    $('pre code button').on('click', function() {
+      var $this = $(this)
+        .parent()
+        .get(0);
 
-            // set initial state...
-            $('pre code .clipboard').remove(); 
-            var $inform = null;
-            
-            // if this is output, not cmds, act on  the previous block...
-            if($this.hasClass("language-sh-output")){
-                $this = $this.parent().prev().find('code');                
-            }
-            // if I don't have a $this, I should quit.
-            if($this.length < 1) return false;
+      // set initial state...
+      $('pre code .clipboard').remove();
+      var $inform = null;
 
-            // parse to get the right linebreaks and such...
-            var cmd = getCodeCommand($this[0]);
+      // if I don't have a $this, I should quit.
+      if ($this.length < 1) return false;
 
-            // create the dom input element to do the select/copy then remove...
-            var $copyArea = $('<textarea class="to-clipboard hide-me" />').appendTo('body').val(cmd);
-            $copyArea[0].select();
-            document.execCommand('copy');
-            $copyArea.remove();
-            $copyArea = null;
+      // parse to get the right linebreaks and such...
+      var cmd = getCodeCommand($this);
 
-            // notify the user of the action and set the GUI state...
-            $inform = $('<div class="clipboard">Copied command to clipboard</div>').appendTo($this);
-            setTimeout(function(){ $inform.addClass("fade") },100);
+      // create the dom input element to do the select/copy then remove...
+      var $copyArea = $('<textarea class="to-clipboard hide-me" />')
+        .appendTo('body')
+        .val(cmd);
+      $copyArea[0].select();
+      document.execCommand('copy');
+      $copyArea.remove();
+      $copyArea = null;
 
-            // clean-up...
-            setTimeout(function(){ $inform.remove();}, 3000);
+      // notify the user of the action and set the GUI state...
+      $inform = $(
+        '<div class="clipboard">Copied command to clipboard</div>'
+      ).appendTo($this);
+      setTimeout(function() {
+        $inform.addClass('fade');
+      }, 100);
 
-        });
+      // clean-up...
+      setTimeout(function() {
+        $inform.remove();
+      }, 3000);
+    });
 
-        function getCodeCommand(block) {
-            const inner = block.innerText
-            const lines = inner.split("\n")
-            let cmd = ""
-            for (let line of lines) {
-                if (line.startsWith("$ ")) {
-                    line = line.substring(2)
-                }
-                cmd += line
-                if (!line.endsWith(" \\")) {
-                    return cmd
-                }
-                cmd += "\n"
-            }
-            return cmd
+    function getCodeCommand(block) {
+      const inner = block.innerText;
+      const lines = inner.split('\n');
+      let cmd = '';
+      for (let line of lines) {
+        if (line.startsWith('$ ')) {
+          line = line.substring(2);
         }
-    };
+        cmd += line;
+        if (!line.endsWith(' \\')) {
+          return cmd;
+        }
+        cmd += '\n';
+      }
+      return cmd;
+    }
+  }
 
-    clipboardMagic();
-
+  clipboardMagic();
 });
