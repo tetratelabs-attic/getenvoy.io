5cf3a64cfcae15fcd56e130c090f9e41539e16fb
Adrian Cole <adrian@tetrate.io>
2021-07-06 14:34:04 +0800

    Fixes drifts from func-e site
    Signed-off-by: Adrian Cole <adrian@tetrate.io>

--- .github/check-usage.sh
+++ .github/check-usage.sh
@@ -2,7 +2,7 @@
 
 # Ensure CLI docs match latest func-e docs
 usage_doc=$1
-curl -sSL https://raw.githubusercontent.com/tetratelabs/func-e/master/site/usage.md > "${usage_doc}"
+curl -sSL https://raw.githubusercontent.com/tetratelabs/func-e/master/USAGE.md > "${usage_doc}"
 
 if [ "$(git status --porcelain -uno)" != "" ]; then
   echo "${usage_doc} needs to be updated to match tetratelabs/func-e:"
